37e3a8cf5dcb72a16ad51f827f0ff1a32e981dfc
hp <vinayakmalviya21@gmail.com>
2023-07-28 15:55:37 +0530

    Workshop section edited

--- src/Components/components/Workshop.jsx
+++ src/Components/components/Workshop.jsx
@@ -1,18 +1,19 @@
 import React from "react";
-import Feature1 from '../images/Feature1.png'
-import Feature3 from '../images/Feature3.png'
+import Session1 from '../images/session1.jpg'
+import Session2 from '../images/session2.jpg'
+import '../css/Workshop.css'
 
 export default function Workshop() {
   return (
     <>
-      <div className="flex flex-col md:flex-row justify-evenly items-center mx-auto w-5/6 bg-[#1e1b4b] rounded-2xl p-4">
+      <div className="flex flex-col md:flex-row justify-evenly items-center mx-auto w-11/12 bg-[#1e1b4b] rounded-2xl p-4">
 
         <div className="flex flex-col justify-center">
 
           <h1 className="text-5xl font-bold pl-8 mt-0 sm:mt-5 text-white">
             Workshops
           </h1>
-          <p className="p-8 text-white font-medium">
+          <p className="p-8 text-white font-medium max-w-xl">
             100+ community-contributed,self-guided coding tutorials and
             ideas.Learn to code by building,one project at a time. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
           </p>
@@ -21,7 +22,7 @@ export default function Workshop() {
             <button className="bg-white ml-8  text-cyan-600 font-bold py-3 px-5 rounded-full">
               <a href="/">Browse a Workshops</a>
             </button>
-            <button className="backdrop-blur-sm bg-white/30 ml-8 mt-2 mb-20 text-white font-bold py-3 px-5 rounded-full">
+            <button className="backdrop-blur-sm bg-white/30 ml-8 mt-2 mb-20 text-white font-bold py-3 px-7 rounded-full">
               <a href="/">Build a Workshops</a>
             </button>
           </div>
@@ -30,24 +31,22 @@ export default function Workshop() {
 
         <div className="flex flex-col sm:flex-row justify-between items-center ">
           
-          <div className="flex flex-col bg-white  text-slate-800 rounded-xl pt-2 mr-0 sm:mr-4">
-            <h2 className="text-xl font-semibold text-black pl-4 ">Event 1</h2>
-            <p className="pl-4 leading-6">
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum,
-              corporis!
-            </p>
-            <img src={Feature1} alt="" className="h-64 sm:h-48 pt-4 rounded-xl" />
-          </div>
-          
-          <div className="flex flex-col bg-white text-slate-800 rounded-xl pt-2 mt-4 sm:mt-0">
-            <h2 className="text-xl font-semibold text-black pl-4 ">Event 2</h2>
-            <p className="pl-4 leading-6">
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum,
-              corporis!
-            </p>
-            <img src={Feature3} alt="" className="h-64 sm:h-48 pt-4 rounded-xl" />
+          <div className="session flex flex-col bg-white text-slate-800 rounded-xl pt-2  mr-0 sm:mr-4 w-72 h-80">
+            <h2 className="text-xl font-semibold text-black pl-4 ">Panel Discussion</h2>
+            {/* <p className="pl-4 leading-6">
+            Ignyte session under Anjali Chouhan(GATE-AIR 13) and Umang Sandhwani(GATE-AIR 200). 
+            </p> */}
+            <img src={Session2} alt="" className="session_img h-72 pt-4 rounded-xl" />
           </div>
 
+          <div className="session flex flex-col bg-white  text-slate-800 rounded-xl pt-2 mt-4 sm:mt-0 w-72 h-80">
+            <h2 className="text-xl font-semibold text-black pl-4 ">Placement Guidance</h2>
+            {/* <p className="pl-4 leading-6">
+            Ignyte session under Mr. Gaurav Tiwari, founder of Sai Campus Recruitment Training. 
+            </p> */}
+            <img src={Session1} alt="" className="session_img h-72 pt-4 rounded-xl" />
+          </div>
+          
         </div>
 
       </div>
