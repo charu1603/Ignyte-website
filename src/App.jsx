87336b37d36baa3a68f70f18d32fc02c8fe1aafb
rohits-web03 <rohit.dey302003@gmail.com>
2024-01-04 22:16:20 +0530

    Moved all the components from main.jsx to App.jsx to make main.jsx cleaner

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
-
-
-import './App.css'
+import Navbar from "./Components/components/Navbar";
+import Hero from "./Components/components/Hero";
+import Card from "./Components/components/Card";
+import About from "./Components/components/About";
+import Feature from "./Components/components/Feature";
+import Workshop from "./Components/components/Workshop";
+import Opensource from "./Components/components/Opensource";
+import Footer from "./Components/components/Footer";
+import './App.css';
 
 function App() {
- 
-
- 
+    return(
+        <>
+            <Navbar />
+            <Hero />
+            <Card />
+            <About />
+            <Feature />
+            <Workshop/>
+            <Opensource />
+            <Footer />
+        </>
+    )
 }
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
+import App from "./App";
 import './index.css'
-import App from './App'
-import Navbar from './Components/components/Navbar';
-import Hero from './Components/components/Hero';
-import About from './Components/components/About';
-import Feature from './Components/components/Feature';
-import Card from './Components/components/Card';
-import Opensource from './Components/components/Opensource';
-import Footer from './Components/components/Footer';
-import Workshop from './Components/components/Workshop';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-  <App />
-  <Navbar />
-  <Hero />
-  <Card />
-  <About />
-  <Feature />
-  <Workshop/>
-  <Opensource />
-  <Footer />
-
+    <App />
   </React.StrictMode>,
 )
