650b99f001f22b236e7076b5a00faedd7e01ec12
Chaitrali kakde <96014425+charu1603@users.noreply.github.com>
2024-01-06 19:22:20 +0530

    Revert "Moved all the components imports from main.jsx to App.jsx"

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from "./App";
+
 import './index.css'
+import App from './App'
+import Navbar from './Components/components/Navbar';
+import Hero from './Components/components/Hero';
+import About from './Components/components/About';
+import Feature from './Components/components/Feature';
+import Card from './Components/components/Card';
+import Opensource from './Components/components/Opensource';
+import Footer from './Components/components/Footer';
+import Workshop from './Components/components/Workshop';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+  <App />
+  <Navbar />
+  <Hero />
+  <Card />
+  <About />
+  <Feature />
+  <Workshop/>
+  <Opensource />
+  <Footer />
+
   </React.StrictMode>,
 )
