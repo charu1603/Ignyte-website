96850b73e5bfb11ab75f35a2d3ee666f04d7b07b
Vedansh Jain <vedansh.code@gmail.com>
2024-02-28 19:45:33 +0530

    Adding Icons

--- src/Components/components/Hero.jsx
+++ src/Components/components/Hero.jsx
@@ -5,7 +5,7 @@ gsap.registerPlugin(ScrollTrigger);
 import bg1 from "../images/bg1.png";
 import bg2 from "../images/bg2.png";
 import bg5 from "../images/bg5.png";
-import { MdStart } from "react-icons/md";
+import { MdStart } from "react-icons/md"; // Icon
 
 
 export default function Hero() {
